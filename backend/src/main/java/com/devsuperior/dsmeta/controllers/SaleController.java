29b98ab0ec99d74d4b7b0ad9ce69cc830000fd80
Adriano Nascimento <silvadriano@gmail.com>
2022-07-14 19:53:39 -0300

    Twilio SMS - config mensagem por SMS sobre vendas do funcionario

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -20,6 +22,9 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
@@ -27,4 +32,34 @@ public class SaleController {
 			Pageable pageable) {   /*Método criado para disponibilizar as vendas para o frontend*/
 		return service.findSales(minDate, maxDate, pageable);
 	}
+
+	@GetMapping("/{id}/nofification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* 
+	 
+	ANOTAÇÕES
+	=========
+	
+	- LINHAS 25, 35 a 36 => Mostra a variável criada (linha 25) com a função (linhas 35 e 36)
+	para chamar o método ENVIA SMS, constado na classe SmsService.java criada dentro do pacote
+	DSMETA.SERVICES.
+	 
+	- LINHA 35 mencionamos um id (entre chaves), que ao ser gerado o SMS, vai emitir a venda,
+	e nome do vendedor 
+	 
+	 
+	 */
 }
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service                  //Está registrada como componente do Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get(); // mostra ID do vendedor (nome vendedor)
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear(); //mostra data da venda na mensagem SMS
+		
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
+		+ " com um total de R$ " + String.format("%.2f", sale.getAmount()); // LINHA 37, 38 - mensagem da venda dentro da função que vai por SMS
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
+
+
+
+
+
+
+
+
+/*ANOTAÇÕES
+ ==========
+ 
+ LINHA 13 a 22 => Dentro da anotation @VALUE aqui temos 4 dados:
+ 
+ - 4 Strings com a anotation @Value com nomes dentro das chaves, que representam os campos
+ mostrados na src/main/resources/aplication.properties (LINHAS 1 a 4).
+ 
+ - Estes nomes referenciam os valores sensíveis disponíveis dentro da conta que abrimos no 
+ Twilio, e que não devem ser repassados.
+ 
+ LINHAS 25 a 34 => Programa envio de SMS pelo java, disponível na documentação da Twílio.
+ 
+ 
+ */
+ 
